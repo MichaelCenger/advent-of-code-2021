eff1170bd936eff8af6b260ca0d95d7c0f25f358
MichaelCenger <MichaelCenger@gmail.com>
2021-12-01 07:17:41 +0100

    Finish Day 1

---
+++ 01_1/_01_1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _01_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var input = ReadInput();
+			int previousNumber = int.MaxValue;
+			int increasingCount = 0;
+			foreach(var number in input)
+			{
+				if(number > previousNumber)
+				{
+					increasingCount++;
+				}
+				previousNumber = number;
+			}
+			Console.WriteLine(increasingCount);
+		}
+
+		private static List<int> ReadInput()
+		{
+			List<int> input = new List<int>();
+			string[] lines = File.ReadAllLines(@"input.txt");
+
+			foreach (string line in lines)
+			{
+				input.Add(Convert.ToInt32(line));
+			}
+
+			return input;
+		}
+	}
+}
---
+++ 01_2/_01_2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _01_2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var input = ReadInput();
+
+			List<int> tripletSums = new List<int>();
+			for (int i = 0; i < input.Count; i++)
+			{
+				if ((i + 2) < input.Count)
+				{
+					tripletSums.Add(input[i] + input[i + 1] + input[i + 2]);
+				}
+			}
+
+			int previousNumber = int.MaxValue;
+			int increasingCount = 0;
+			foreach (var number in tripletSums)
+			{
+				if (number > previousNumber)
+				{
+					increasingCount++;
+				}
+				previousNumber = number;
+			}
+
+			Console.WriteLine(increasingCount);
+		}
+
+		private static List<int> ReadInput()
+		{
+			List<int> input = new List<int>();
+			string[] lines = File.ReadAllLines(@"input.txt");
+
+			foreach (string line in lines)
+			{
+				input.Add(Convert.ToInt32(line));
+			}
+
+			return input;
+		}
+	}
+}
