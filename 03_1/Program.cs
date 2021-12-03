4faed3543cca2ef6cfdade4a9abdb0dbaa9dd0b6
MichaelCenger <MichaelCenger@gmail.com>
2021-12-03 09:18:35 +0100

    Finish day 3

--- 03_1/Program.cs
+++ 03_1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace _03_1
 {
@@ -9,19 +10,40 @@ namespace _03_1
 		static void Main(string[] args)
 		{
 			var input = ReadInput();
+			string gamma = "", epsilon = "";
+			for (int i = 0; i < input[0].Length; i++)
+			{
+				int count1 = 0, count0 = 0;
+				for (int j = 0; j < input.Count; j++)
+				{
+					if (input[j][i] == '0')
+					{
+						count0++;
+					}
+					else
+					{
+						count1++;
+					}
+				}
+				if (count1 > count0)
+				{
+					gamma += '1';
+					epsilon += '0';
+				}
+				else
+				{
+					gamma += '0';
+					epsilon += '1';
+				}
+			}
+			int a= Convert.ToInt32(gamma, 2); 
+			int b= Convert.ToInt32(epsilon, 2);
+			Console.WriteLine(a * b);
 		}
 
-		private static List<int> ReadInput()
+		private static List<string> ReadInput()
 		{
-			List<int> input = new List<int>();
-			string[] lines = File.ReadAllLines(@"input.txt");
-
-			foreach (string line in lines)
-			{
-				input.Add(Convert.ToInt32(line));
-			}
-
-			return input;
+			return File.ReadAllLines(@"input.txt").ToList();
 		}
 	}
 }
--- 03_2/Program.cs
+++ 03_2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace _03_2
 {
@@ -9,19 +10,107 @@ namespace _03_2
 		static void Main(string[] args)
 		{
 			var input = ReadInput();
+			var inputCopy = new List<string>(input);
+			int index = 0;
+			while (input.Count != 1)
+			{
+				string mostCommon = "0", leastCommon = "0";
+				int count1 = 0, count0 = 0;
+				for (int j = 0; j < input.Count; j++)
+				{
+					if (input[j][index] == '0')
+					{
+						count0++;
+					}
+					else
+					{
+						count1++;
+					}
+				}
+				if (count1 > count0)
+				{
+					mostCommon = "1";
+				}
+				else if (count1 < count0)
+				{
+					leastCommon = "1";
+				}
+				else
+				{
+					mostCommon = "1";
+				}
+				var ToRemove = new List<int>();
+				for (int k = 0; k < input.Count; k++)
+				{
+					if (input[k][index] != mostCommon[0])
+					{
+						ToRemove.Add(k);
+					}
+				}
+				ToRemove.Reverse();
+				foreach (var r in ToRemove)
+				{
+					input.RemoveAt(r);
+				}
+
+				index++;
+			}
+			int a = Convert.ToInt32(input[0], 2);
+
+			input = inputCopy;
+
+			index = 0;
+			while (input.Count != 1)
+			{
+				string mostCommon = "0", leastCommon = "0";
+				int count1 = 0, count0 = 0;
+				for (int j = 0; j < input.Count; j++)
+				{
+					if (input[j][index] == '0')
+					{
+						count0++;
+					}
+					else
+					{
+						count1++;
+					}
+				}
+				if (count1 > count0)
+				{
+					mostCommon = "1";
+				}
+				else if (count1 < count0)
+				{
+					leastCommon = "1";
+				}
+				else
+				{
+					leastCommon = "0";
+				}
+				var ToRemove = new List<int>();
+				for (int k = 0; k < input.Count; k++)
+				{
+					if (input[k][index] != leastCommon[0])
+					{
+						ToRemove.Add(k);
+					}
+				}
+				ToRemove.Reverse();
+				foreach (var r in ToRemove)
+				{
+					input.RemoveAt(r);
+				}
+
+				index++;
+			}
+			int b = Convert.ToInt32(input[0], 2);
+			Console.WriteLine(b);
+			Console.WriteLine(a * b);
 		}
 
-		private static List<int> ReadInput()
+		private static List<string> ReadInput()
 		{
-			List<int> input = new List<int>();
-			string[] lines = File.ReadAllLines(@"input.txt");
-
-			foreach (string line in lines)
-			{
-				input.Add(Convert.ToInt32(line));
-			}
-
-			return input;
+			return File.ReadAllLines(@"input.txt").ToList();
 		}
 	}
 }
