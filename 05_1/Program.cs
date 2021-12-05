6de9ec3adb72a620a22ed4d67f5aee8f5ad011f7
MichaelCenger <MichaelCenger@gmail.com>
2021-12-05 10:09:38 +0100

    Day 5

--- 05_1/Program.cs
+++ 05_1/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace _05_1
 {
@@ -9,19 +11,97 @@ namespace _05_1
 		static void Main(string[] args)
 		{
 			var input = ReadInput();
-		}
-
-		private static List<int> ReadInput()
-		{
-			List<int> input = new List<int>();
-			string[] lines = File.ReadAllLines(@"input.txt");
-
-			foreach (string line in lines)
+			Dictionary<(int x, int y), int> coordinates = new Dictionary<(int x, int y), int>();
+			foreach(var line in ReadInput())
 			{
-				input.Add(Convert.ToInt32(line));
+				var splitLine = Regex.Replace(line, " -> ", ",").Split(",");
+				(int x, int y) start = (Convert.ToInt32(splitLine[0]), Convert.ToInt32(splitLine[1]));
+				(int x, int y) end = (Convert.ToInt32(splitLine[2]), Convert.ToInt32(splitLine[3]));
+				if(start.x != end.x && start.y != end.y)
+				{
+					continue;
+				}
+				if(start.x > end.x)
+				{
+					for (int x = end.x; x <= start.x; x++)
+					{
+						var pos = (x, start.y);
+						if (!coordinates.ContainsKey(pos))
+						{
+							coordinates.Add(pos, 1);
+						}
+						else
+						{
+							coordinates[pos]++;
+						}
+					}
+					continue;
+				}
+				else if(start.x < end.x)
+				{
+					for (int x = start.x; x <= end.x; x++)
+					{
+						var pos = (x, start.y);
+						if (!coordinates.ContainsKey(pos))
+						{
+							coordinates.Add(pos, 1);
+						}
+						else
+						{
+							coordinates[pos]++;
+						}
+					}
+					continue;
+				}
+				if (start.y > end.y)
+				{
+					for (int y = end.y; y <= start.y; y++)
+					{
+						var pos = (start.x, y);
+						if (!coordinates.ContainsKey(pos))
+						{
+							coordinates.Add(pos, 1);
+						}
+						else
+						{
+							coordinates[pos]++;
+						}
+					}
+					continue;
+				}
+				else if (start.y < end.y)
+				{
+					for (int y = start.y; y <= end.y; y++)
+					{
+						var pos = (start.x, y);
+						if (!coordinates.ContainsKey(pos))
+						{
+							coordinates.Add(pos, 1);
+						}
+						else
+						{
+							coordinates[pos]++;
+						}
+					}
+					continue;
+				}
+
 			}
 
-			return input;
+			int sum = 0;
+			foreach(var entry in coordinates)
+			{
+				if (entry.Value >= 2)
+				{
+					sum++;
+				}
+			}
+			Console.WriteLine(sum);
+		}
+
+		private static List<string> ReadInput()
+		{
+			return File.ReadAllLines(@"input.txt").ToList();
 		}
 	}
 }
--- 05_2/Program.cs
+++ 05_2/Program.cs
@@ -9,19 +9,129 @@ namespace _05_2
 		static void Main(string[] args)
 		{
 			var input = ReadInput();
-		}
-
-		private static List<int> ReadInput()
-		{
-			List<int> input = new List<int>();
-			string[] lines = File.ReadAllLines(@"input.txt");
-
-			foreach (string line in lines)
+			Dictionary<(int x, int y), int> coordinates = new Dictionary<(int x, int y), int>();
+			foreach (var line in ReadInput())
 			{
-				input.Add(Convert.ToInt32(line));
+				var splitLine = Regex.Replace(line, " -> ", ",").Split(",");
+				(int x, int y) start = (Convert.ToInt32(splitLine[0]), Convert.ToInt32(splitLine[1]));
+				(int x, int y) end = (Convert.ToInt32(splitLine[2]), Convert.ToInt32(splitLine[3]));
+				if (start.x > end.x)
+				{
+					int y = end.y;
+					for (int x = end.x; x <= start.x; x++)
+					{
+						var pos = (x, y);
+						if (!coordinates.ContainsKey(pos))
+						{
+							coordinates.Add(pos, 1);
+						}
+						else
+						{
+							coordinates[pos]++;
+						}
+						if (start.y > end.y)
+						{
+							y++;
+						}
+						else if (start.y < end.y)
+						{
+							y--;
+						}
+					}
+					continue;
+				}
+				else if (start.x < end.x)
+				{
+					int y = start.y;
+					for (int x = start.x; x <= end.x; x++)
+					{
+						var pos = (x, y);
+						if (!coordinates.ContainsKey(pos))
+						{
+							coordinates.Add(pos, 1);
+						}
+						else
+						{
+							coordinates[pos]++;
+						}
+						if (start.y > end.y)
+						{
+							y--;
+						}
+						else if (start.y < end.y)
+						{
+							y++;
+						}
+					}
+					continue;
+				}
+				if (start.y > end.y)
+				{
+					int x = end.x;
+					for (int y = end.y; y <= start.y; y++)
+					{
+						var pos = (x, y);
+						if (!coordinates.ContainsKey(pos))
+						{
+							coordinates.Add(pos, 1);
+						}
+						else
+						{
+							coordinates[pos]++;
+						}
+						if (start.x > end.x)
+						{
+							x++;
+						}
+						else if (start.x < end.x)
+						{
+							x--;
+						}
+					}
+					continue;
+				}
+				else if (start.y < end.y)
+				{
+					int x = start.x;
+					for (int y = start.y; y <= end.y; y++)
+					{
+						var pos = (x, y);
+						if (!coordinates.ContainsKey(pos))
+						{
+							coordinates.Add(pos, 1);
+						}
+						else
+						{
+							coordinates[pos]++;
+						}
+						if (start.x > end.x)
+						{
+							x--;
+						}
+						else if (start.x < end.x)
+						{
+							x++;
+						}
+					}
+					continue;
+				}
 			}
 
-			return input;
+
+			int sum = 0;
+			foreach (var entry in coordinates)
+			{
+				if (entry.Value >= 2)
+				{
+					sum++;
+				}
+			}
+			Console.WriteLine(sum);
+		}
+
+		private static List<string> ReadInput()
+		{
+			return File.ReadAllLines(@"input.txt").ToList();
 		}
 	}
 }
