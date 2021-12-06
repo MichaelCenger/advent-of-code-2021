f6be615efd5059752d2f025215adeff9fc09f32c
MichaelCenger <MichaelCenger@gmail.com>
2021-12-06 06:31:23 +0100

    Day 6

--- 06_1/Program.cs
+++ 06_1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace _06_1
 {
@@ -9,19 +10,33 @@ namespace _06_1
 		static void Main(string[] args)
 		{
 			var input = ReadInput();
+			List<int> fish = new List<int>();
+			foreach(var num in input)
+			{
+				fish.Add(Convert.ToInt32(num)); 
+			}
+			for ( int i = 0;i < 80; i++)
+			{
+				var fishToAdd = new List<int>();
+				for(int j = 0; j< fish.Count;j++)
+				{
+					int num = fish[j];
+					num--;
+					if (num == -1)
+					{
+						num = 6;
+						fishToAdd.Add(8);
+					}
+					fish[j] = num;
+				}
+				fish.AddRange(fishToAdd);
+			}
+			Console.WriteLine(fish.Count);
 		}
 
-		private static List<int> ReadInput()
+		private static List<string> ReadInput()
 		{
-			List<int> input = new List<int>();
-			string[] lines = File.ReadAllLines(@"input.txt");
-
-			foreach (string line in lines)
-			{
-				input.Add(Convert.ToInt32(line));
-			}
-
-			return input;
+			return File.ReadAllText(@"input.txt").Split(",").ToList();
 		}
 	}
 }
--- 06_2/Program.cs
+++ 06_2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace _06_2
 {
@@ -9,19 +10,45 @@ namespace _06_2
 		static void Main(string[] args)
 		{
 			var input = ReadInput();
+			Dictionary<int, long> fish = new Dictionary<int, long>();
+			fish.Add(0, 0);
+			fish.Add(1, 0);
+			fish.Add(2, 0);
+			fish.Add(3, 0);
+			fish.Add(4, 0);
+			fish.Add(5, 0);
+			fish.Add(6, 0);
+			fish.Add(7, 0);
+			fish.Add(8, 0);
+			foreach (var num in input)
+			{
+				fish[Convert.ToInt32(num)]++;
+			}
+			for (int i = 0; i < 256; i++)
+			{
+				var copy = new Dictionary<int, long>(fish);
+				copy[0] = fish[1];
+				copy[1] = fish[2];
+				copy[2] = fish[3];
+				copy[3] = fish[4];
+				copy[4] = fish[5];
+				copy[5] = fish[6];
+				copy[6] = fish[7] + fish[0];
+				copy[7] = fish[8];
+				copy[8] = fish[0];
+				fish = new Dictionary<int, long>(copy);
+			}
+			long sum = 0;
+			foreach (var f in fish)
+			{
+				sum += f.Value;
+			}
+			Console.WriteLine(sum);
 		}
 
-		private static List<int> ReadInput()
+		private static List<string> ReadInput()
 		{
-			List<int> input = new List<int>();
-			string[] lines = File.ReadAllLines(@"input.txt");
-
-			foreach (string line in lines)
-			{
-				input.Add(Convert.ToInt32(line));
-			}
-
-			return input;
+			return File.ReadAllText(@"input.txt").Split(",").ToList();
 		}
 	}
 }
