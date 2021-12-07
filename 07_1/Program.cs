7469752f2f2d972d7a6dd127fbb5aae0212829de
MichaelCenger <MichaelCenger@gmail.com>
2021-12-07 06:45:18 +0100

    Day 7

--- 07_1/Program.cs
+++ 07_1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace _07_1
 {
@@ -9,19 +10,33 @@ namespace _07_1
 		static void Main(string[] args)
 		{
 			var input = ReadInput();
+			var positions = new List<int>();
+			foreach (var pos in input)
+			{
+				positions.Add(Convert.ToInt32(pos));
+			}
+			int leastFuel = int.MaxValue;
+			int leastFuelPosition = 0;
+			foreach(var target in positions)
+			{
+				int currentTotalFuel = 0;
+				foreach(var pos in positions)
+				{
+					currentTotalFuel += Math.Abs(target - pos);
+					
+				}
+				if(currentTotalFuel < leastFuel)
+				{
+					leastFuel = currentTotalFuel;
+					leastFuelPosition = target;
+				}
+			}
+			Console.WriteLine(leastFuel);
 		}
 
-		private static List<int> ReadInput()
+		private static List<string> ReadInput()
 		{
-			List<int> input = new List<int>();
-			string[] lines = File.ReadAllLines(@"input.txt");
-
-			foreach (string line in lines)
-			{
-				input.Add(Convert.ToInt32(line));
-			}
-
-			return input;
+			return File.ReadAllText(@"input.txt").Split(",").ToList();
 		}
 	}
 }
--- 07_2/Program.cs
+++ 07_2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace _07_2
 {
@@ -9,19 +10,42 @@ namespace _07_2
 		static void Main(string[] args)
 		{
 			var input = ReadInput();
+			var positions = new List<int>();
+			int maxNum = 0;
+			foreach (var pos in input)
+			{
+				positions.Add(Convert.ToInt32(pos));
+				if (Convert.ToInt32(pos) > maxNum)
+				{
+					maxNum = Convert.ToInt32(pos);
+				}
+			}
+			int leastFuel = int.MaxValue;
+			int leastFuelPosition = 0;
+			for (int target = 0; target <= maxNum; target++)
+			{
+				int currentTotalFuel = 0;
+				foreach (var pos in positions)
+				{
+					int cost = 0;
+					for (int j = 0; j <= Math.Abs(target - pos); j++)
+					{
+						cost += j;
+					}
+					currentTotalFuel += cost;
+				}
+				if (currentTotalFuel < leastFuel)
+				{
+					leastFuel = currentTotalFuel;
+					leastFuelPosition = target;
+				}
+			}
+			Console.WriteLine(leastFuel);
 		}
 
-		private static List<int> ReadInput()
+		private static List<string> ReadInput()
 		{
-			List<int> input = new List<int>();
-			string[] lines = File.ReadAllLines(@"input.txt");
-
-			foreach (string line in lines)
-			{
-				input.Add(Convert.ToInt32(line));
-			}
-
-			return input;
+			return File.ReadAllText(@"input.txt").Split(",").ToList();
 		}
 	}
 }
