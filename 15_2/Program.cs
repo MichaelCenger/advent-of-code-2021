590d6fea7ddcd5fc6740fd373232fa12e7bf9806
MichaelCenger <MichaelCenger@gmail.com>
2021-12-13 07:33:10 +0100

    Templates

---
+++ 15_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _15_2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var input = ReadInput();
+		}
+
+		private static List<int> ReadInput()
+		{
+			List<int> input = new List<int>();
+			string[] lines = File.ReadAllLines(@"input.txt");
+
+			foreach (string line in lines)
+			{
+				input.Add(Convert.ToInt32(line));
+			}
+
+			return input;
+		}
+	}
+}
