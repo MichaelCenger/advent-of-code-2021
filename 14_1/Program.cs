8a94de031fe9accdf7353000b664bdbb2d5e31df
MichaelCenger <MichaelCenger@gmail.com>
2021-12-14 10:02:38 +0100

    Day 14

--- 14_1/Program.cs
+++ 14_1/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Text;
+using System.Timers;
 
 namespace _14_1
 {
@@ -10,12 +13,88 @@ namespace _14_1
 		static void Main(string[] args)
 		{
 			var input = ReadInput();
-			foreach(var line in input)
+			Dictionary<char, long> Count = new Dictionary<char, long>();
+			string polymer = input[0];
+			Dictionary<string, char> instructions = new Dictionary<string, char>();
+			for (int i = 2; i < input.Count; i++)
 			{
+				string searchTerm = input[i].Split("->")[0].Trim();
+				char replaceWith = input[i].Split("->")[1].Trim()[0];
+				instructions.Add(searchTerm,replaceWith);
+			}
+			for (int k = 0; k < 10; k++)
+			{
+				Dictionary<int, char> toInsert = new Dictionary<int, char>();
+				Stopwatch sw = new Stopwatch();
+				sw.Start();
+				for (int l = 0; l < polymer.Length - 1; l++)
+				{
+					string s = polymer[l] + "" + polymer[l + 1];
+					if (instructions.ContainsKey(s))
+					{
+						toInsert.Add(l + 1, instructions[s]);
+						if (!Count.ContainsKey(instructions[s]))
+						{
+							Count.Add(instructions[s], 1);
+						}
+						else
+						{
+							Count[instructions[s]]++;
+						}
+					}
+				}
+				StringBuilder newPolymer = new StringBuilder(polymer.Length + toInsert.Count);
+
+				for (int i = 0; i < polymer.Length + toInsert.Count; i++)
+				{
+					newPolymer.Append('X');
+
+				}
+
+				int offset = 0;
+				for (int i = 0; i < polymer.Length + toInsert.Count; i++)
+				{
+					if (toInsert.ContainsKey(i))
+					{
+						newPolymer[i + offset] = toInsert[i];
+						offset++;
+					}
+				}
+
+				int originalIndex = 0;
+				for (int i = 0; i < newPolymer.Length; i++)
+				{
+					if (newPolymer[i] == 'X')
+					{
+						newPolymer[i] = polymer[originalIndex];
+						originalIndex++;
+					}
+				}
+				polymer = newPolymer.ToString();
+				Console.WriteLine(k);
 
 			}
+
+			for (int i = 0; i < input[0].Length; i++)
+			{
+				Count[input[0][i]]++;
+			}
 			long result = 0;
-			Console.Write(result);
+
+			long smallest = Int32.MaxValue;
+			long largest = Int32.MinValue;
+			foreach (var c in Count)
+			{
+				if (c.Value < smallest)
+				{
+					smallest = c.Value;
+				}
+				if (c.Value > largest)
+				{
+					largest = c.Value;
+				}
+			}
+			Console.Write(largest - smallest);
 		}
 
 		private static List<string> ReadInput()
--- 14_2/Program.cs
+++ 14_2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace _14_2
 {
@@ -9,19 +10,124 @@ namespace _14_2
 		static void Main(string[] args)
 		{
 			var input = ReadInput();
-		}
-
-		private static List<int> ReadInput()
-		{
-			List<int> input = new List<int>();
-			string[] lines = File.ReadAllLines(@"input.txt");
-
-			foreach (string line in lines)
+			Dictionary<char, long> Count = new Dictionary<char, long>();
+			Dictionary<string, char> instructions = new Dictionary<string, char>();
+			for (int i = 2; i < input.Count; i++)
 			{
-				input.Add(Convert.ToInt32(line));
+				string searchTerm = input[i].Split("->")[0].Trim();
+				char replaceWith = input[i].Split("->")[1].Trim()[0];
+				instructions.Add(searchTerm, replaceWith);
+			}
+			Dictionary<string, long> pairs = new Dictionary<string, long>();
+			for (int l = 0; l < input[0].Length - 1; l++)
+			{
+				string s = input[0][l] + "" + input[0][l + 1];
+				if (!pairs.ContainsKey(s))
+				{
+					pairs.Add(s, 1);
+				}
+				else
+				{
+					pairs[s]++;
+				}
+				if (Count.ContainsKey(input[0][l]))
+				{
+					Count[input[0][l]]++;
+				}
+				else
+				{
+					Count.Add(input[0][l], 1);
+				}
+			}
+			if (Count.ContainsKey(input[0][19]))
+			{
+				Count[input[0][19]]++;
+			}
+			else
+			{
+				Count.Add(input[0][19], 1);
 			}
 
-			return input;
+			for (int k = 0; k < 40; k++)
+			{
+				Dictionary<string, long> newPairs = new Dictionary<string, long>(pairs);
+				List<(string, long)> toRemove = new List<(string, long)>();
+				foreach (var instruction in instructions)
+				{
+					if (pairs.ContainsKey(instruction.Key))
+					{
+						long currentCount = pairs[instruction.Key];
+
+						if (Count.ContainsKey(instruction.Value))
+						{
+							Count[instruction.Value] += currentCount;
+						}
+						else
+						{
+							Count.Add(instruction.Value, currentCount);
+						}
+						string s1 = instruction.Key[0] + "" + instruction.Value;
+						if (newPairs.ContainsKey(s1))
+						{
+							newPairs[s1] += currentCount;
+						}
+						else
+						{
+							newPairs.Add(s1, currentCount);
+						}
+						string s2 = instruction.Value + "" + instruction.Key[1];
+						if (newPairs.ContainsKey(s2))
+						{
+							newPairs[s2] += currentCount;
+						}
+						else
+						{
+							newPairs.Add(s2, currentCount);
+						}
+						toRemove.Add((instruction.Key, currentCount));
+					}
+				}
+				for (int i = 0; i < toRemove.Count; i++)
+				{
+					newPairs[toRemove[i].Item1] -= toRemove[i].Item2;
+					if (newPairs[toRemove[i].Item1] == 0)
+					{
+						newPairs.Remove(toRemove[i].Item1);
+					}
+				}
+				pairs = newPairs;
+			}
+
+
+			long smallest = long.MaxValue;
+			long largest = 0;
+			foreach (var c in Count)
+			{
+				Console.WriteLine(c.Key + " occurs " + c.Value + " times");
+				if (c.Value < smallest)
+				{
+					smallest = c.Value;
+				}
+				if (c.Value > largest)
+				{
+					largest = c.Value;
+				}
+			}
+			Console.WriteLine((largest - smallest));
+		}
+
+		private static List<string> ReadInput()
+		{
+			return File.ReadAllLines(@"input.txt").ToList();
+			//List<int> input = new List<int>();
+			//string[] lines = File.ReadAllLines(@"input.txt");
+
+			//foreach (string line in lines)
+			//{
+			//	input.Add(Convert.ToInt32(line));
+			//}
+
+			//return input;
 		}
 	}
 }
