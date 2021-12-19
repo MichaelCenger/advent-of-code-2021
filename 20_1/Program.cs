211a0e10855c2002ad3b21bf0be5343ad8cc67d0
MichaelCenger <MichaelCenger@gmail.com>
2021-12-19 16:41:15 +0100

    Templates

---
+++ 20_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _20_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var input = ReadInput();
+			foreach (var line in input)
+			{
+
+			}
+			long result = 0;
+			Console.WriteLine(result);
+		}
+
+		private static List<string> ReadInput()
+		{
+			return File.ReadAllLines(@"input.txt").ToList();
+			//List<int> input = new List<int>();
+			//string[] lines = File.ReadAllLines(@"input.txt");
+
+			//foreach (string line in lines)
+			//{
+			//	input.Add(Convert.ToInt32(line));
+			//}
+
+			//return input;
+		}
+	}
+}
