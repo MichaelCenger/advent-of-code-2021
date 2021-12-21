0eb00051fe7d7da81ffc467716c6eadeeb34cb02
MichaelCenger <MichaelCenger@gmail.com>
2021-12-21 08:06:10 +0100

    Day 21

--- 21_1/Program.cs
+++ 21_1/Program.cs
@@ -7,15 +7,67 @@ namespace _21_1
 {
 	class Program
 	{
+		static Random rng = new Random();
 		static void Main(string[] args)
 		{
 			var input = ReadInput();
-			foreach (var line in input)
+			int dieRollCount = 0;
+			int p1Pos = 1;
+			int p2Pos = 2;
+			int p1Score = 0;
+			int p2Score = 0;
+			while (true)
 			{
+				int totalRoll = 0;
+				for (int i = 0; i < 3; i++)
+				{
+					dieRollCount++;
+					int roll = dieRollCount % 100;
+					if (roll == 0)
+					{
+						roll = 100;
+					}
 
+					totalRoll += roll;
+				}
+				int newPos = (p1Pos + totalRoll) % 10;
+				if (newPos == 0)
+				{
+					newPos = 10;
+				}
+				p1Score += newPos;
+				p1Pos = newPos;
+				if (p1Score >= 1000)
+				{
+					Console.WriteLine(p2Score * dieRollCount);
+					break;
+				}
+
+				totalRoll = 0;
+				for (int i = 0; i < 3; i++)
+				{
+					dieRollCount++;
+					int roll = dieRollCount % 100;
+					if (roll == 0)
+					{
+						roll = 100;
+					}
+
+					totalRoll += roll;
+				}
+				newPos = (p2Pos + totalRoll) % 10;
+				if (newPos == 0)
+				{
+					newPos = 10;
+				}
+				p2Score += newPos;
+				p2Pos = newPos;
+				if (p2Score >= 1000)
+				{
+					Console.WriteLine(p1Score * dieRollCount);
+					break;
+				}
 			}
-			long result = 0;
-			Console.WriteLine(result);
 		}
 
 		private static List<string> ReadInput()
--- 21_2/Program.cs
+++ 21_2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace _21_2
 {
@@ -8,20 +9,105 @@ namespace _21_2
 	{
 		static void Main(string[] args)
 		{
-			var input = ReadInput();
-		}
+			long p1Wins = 0;
+			long p2Wins = 0;
+			Dictionary<(int p1Pos, int p2Pos, int p1Score, int p2Score), long> games = new Dictionary<(int p1Pos, int p2Pos, int p1Score, int p2Score), long>();
+			List<int> possibleRolls = new List<int>();
 
-		private static List<int> ReadInput()
-		{
-			List<int> input = new List<int>();
-			string[] lines = File.ReadAllLines(@"input.txt");
-
-			foreach (string line in lines)
+			for (int i = 1; i <= 3; i++)
 			{
-				input.Add(Convert.ToInt32(line));
+				for (int j = 1; j <= 3; j++)
+				{
+					for (int k = 1; k <= 3; k++)
+					{
+						possibleRolls.Add(i + j + k);
+					}
+				}
 			}
 
-			return input;
+			games.Add((1, 2, 0, 0), 1);
+			while (games.Count > 0)
+			{
+				Dictionary<(int p1Pos, int p2Pos, int p1Score, int p2Score), long> gamesCopy = new Dictionary<(int p1Pos, int p2Pos, int p1Score, int p2Score), long>();
+				foreach (var game in games)
+				{
+					foreach (var roll in possibleRolls)
+					{
+						(int p1Pos, int p2Pos, int p1Score, int p2Score) state = game.Key;
+						int newPos = (state.p1Pos + roll) % 10;
+						if (newPos == 0)
+						{
+							newPos = 10;
+						}
+						state.p1Score += newPos;
+						state.p1Pos = newPos;
+
+						if (state.p1Score >= 21)
+						{
+							p1Wins += game.Value;
+						}
+						else
+						{
+							if (gamesCopy.ContainsKey(state))
+							{
+								gamesCopy[state] += game.Value;
+							}
+							else
+							{
+								gamesCopy.Add(state, game.Value);
+							}
+						}
+					}
+				}
+				games = gamesCopy;
+				gamesCopy = new Dictionary<(int p1Pos, int p2Pos, int p1Score, int p2Score), long>();
+				foreach (var game in games)
+				{
+					foreach (var roll in possibleRolls)
+					{
+						(int p1Pos, int p2Pos, int p1Score, int p2Score) state = game.Key;
+						int newPos = (state.p2Pos + roll) % 10;
+						if (newPos == 0)
+						{
+							newPos = 10;
+						}
+						state.p2Score += newPos;
+						state.p2Pos = newPos;
+
+						if (state.p2Score >= 21)
+						{
+							p2Wins += game.Value;
+						}
+						else
+						{
+							if (gamesCopy.ContainsKey(state))
+							{
+								gamesCopy[state] += game.Value;
+							}
+							else
+							{
+								gamesCopy.Add(state, game.Value);
+							}
+						}
+					}
+				}
+				games = gamesCopy;
+			}
+			Console.WriteLine(Math.Max(p1Wins, p2Wins));
+		}
+
+		private static List<string> ReadInput()
+		{
+			return File.ReadAllLines(@"input.txt").ToList();
+			//List<int> input = new List<int>();
+			//string[] lines = File.ReadAllLines(@"input.txt");
+
+			//foreach (string line in lines)
+			//{
+			//	input.Add(Convert.ToInt32(line));
+			//}
+
+			//return input;
 		}
 	}
 }
